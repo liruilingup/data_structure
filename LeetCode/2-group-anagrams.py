d56ed65a5f51230ae9c4ce0e63ed4ad675a97462
liruilingup <1349932557@qq.com>
2025-07-18 23:52:06 +0800

    2025 刷题

--- LeetCode/2-group-anagrams.py
+++ LeetCode/2-group-anagrams.py
@@ -3,14 +3,21 @@ strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
 out = [["bat"],["nat","tan"],["ate","eat","tea"]]
 
 def groupAnagrams(strs):
-	"""
-	49. 字母异位词分组
-	"""
-	print("---")
+    """
+    49. 字母异位词分组
+    """
+    res_dict = {}
+    for str in strs:
+        tmp = "".join(sorted(list(str)))
+        if tmp in res_dict:
+            res_dict[tmp].append(str)
+        else:
+            res_dict[tmp] = [str]
+    
+    result = []
+    for key, value in res_dict.items():
+        result.append(value)
+    return result
 
-	for str in strs:
-		print(sorted(list(str)))
-
-
-groupAnagrams(strs)
+print(groupAnagrams(strs))
 
